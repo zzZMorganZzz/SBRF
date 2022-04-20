40c4f064b80cda655ca23ee57fd3a28872f0fa09
zzZMorganZzz <mikhaylov-kg@mail.ru>
2022-04-20 14:41:36 +1000

    Подготовка площадки проекта

---
+++ TimeManager/TimeManager/TimeManager.Core.DataCore.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimeManager.Core.DataCore.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
